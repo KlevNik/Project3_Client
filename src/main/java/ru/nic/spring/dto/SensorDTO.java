6638f19506167b2b87d9479b2cff36f6c378bcca
Nick <113937068+KlevNik@users.noreply.github.com>
2023-12-20 21:28:16 +0300

    final

---
+++ src/main/java/ru/nic/spring/dto/SensorDTO.java
@@ -0,0 +1,13 @@
+package ru.nic.spring.dto;
+
+public class SensorDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
